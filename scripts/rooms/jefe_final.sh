384ddf7d1eab785ae170c824426c2746f33ef77b
Borja Martinena <bmartinenac@gmail.com>
2023-05-22 13:19:41 +0200

    fix: removed roomscripts

--- scripts/rooms/jefe_final.sh
+++
@@ -1,5 +0,0 @@
-if [[ $SHELL_ADVENTURE_LANG == "es" ]]; then
-  ./jefe_final
-else
-  ./final_boss
-fi
--- scripts/rooms/locked.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $SHELL_ADVENTURE_LANG == "es" ]]; then
-  rm $SHELL_ADVENTURE_ROOT/visited/cerrado  
-  cd entrada
-else
-  rm $SHELL_ADVENTURE_ROOT/visited/locked  
-  cd entrance
-fi
